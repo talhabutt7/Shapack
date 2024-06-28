56f373aa805ebcf3e80bde0398c51f870c0824ec
talhabutt7 <talhabuttt@gmail.com>
2024-06-28 18:15:06 +0100

    Validates Email presences

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
+  validates :email, presence: true
+
   # Method for omniauth to create a user
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
